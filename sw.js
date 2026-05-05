2ebf91cd5de59a418573cef8defb47cd15b1b5f9
Aelysson Souza <Aelysson@andrademarinholmf.com>
2026-05-05 10:35:17 -0300

    ajuste sw

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+const CACHE_NAME = 'sos-mogeiro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './dados.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+// Instalação: baixa os arquivos
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+// Busca: serve do cache se estiver offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
